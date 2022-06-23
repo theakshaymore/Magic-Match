acb905f0af69f4d7107f4e46126769ce36143c8d
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2022-06-23 13:04:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { FronteggProvider } from "@frontegg/react";
-
-const contextOptions = {
-  baseUrl: "https://app-vy70tcs4b00y.frontegg.com",
-  clientId: "2c9d9bd1-e3ff-498d-bf2a-cfab0349c403",
-};
 
 ReactDOM.render(
-  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+  <React.StrictMode>
     <App />
-  </FronteggProvider>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
